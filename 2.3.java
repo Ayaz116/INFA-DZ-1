f7884bdd7120d9038d8d032fbd84ef0ab59e46db
Ayaz <mustafin.a.n@yandex.ru>
2023-09-16 11:46:05 +0300

    Исправил 2.3

--- 2.3.java
+++ 2.3.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner n = new Scanner(System.in);
-        System.out.println("+++");
-        System.out.println("!!!!");
-        System.out.println("??");
+        System.out.println("+++" + "\n" + "!!!!"+ "\n" + "??");
     }
 }
