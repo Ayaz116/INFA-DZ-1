0ebec51063d8b360aea1c0c3a8a5f062e0b63af4
Ayaz <mustafin.a.n@yandex.ru>
2023-09-16 11:43:34 +0300

    Исправил 1.5

--- 1.5.java
+++ 1.5.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner n = new Scanner(System.in);
-        double x = 7.5;
-        double a = x*2, b = x *3;
+        double x = 7.5, a = x*2, b = x *3;
     }
 }
